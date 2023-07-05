572d866d774a10a8a48ec68eea836976c2c4187c
richi-coder <richicoder@gmail.com>
2023-07-04 22:26:39 -0400

    blog page fixed

--- src/BlogFeedPage/Post.jsx
+++ src/BlogFeedPage/Post.jsx
@@ -1,8 +1,8 @@
 function Post({ postData }) {
   return (
-    <div>
-      <div>{postData.title}</div>
-      <div>
+    <div className="flex flex-row items-center justify-center gap-x-5 border-2">
+      <div className="border-r text-center w-1/2">{postData.title}</div>
+      <div className="border-l text-center w-1/2">
         {postData.message}
       </div>
     </div>
--- src/BlogFeedPage/Posts.jsx
+++ src/BlogFeedPage/Posts.jsx
@@ -5,13 +5,13 @@ function Posts({ postsState }) {
     const posts = postsState.map(post => <Post key={uuidv4()} postData={post} />)
 
   return (
-    <>
+    <div className="flex flex-col">
         {
             posts.length > 0 ?
             posts :
             <div>No hay posts para mostrar</div>
         }
-    </>
+    </div>
   )
 }
 
